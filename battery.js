9396028afe4b829f1e9077584c5ccc6f6bee998b
A M I N <102389207+aminrngbr1122@users.noreply.github.com>
2023-12-23 10:38:16 +0330

    battery.js

---
+++ battery.js
@@ -0,0 +1,11 @@
+
+    document.querySelectorAll("Battery").forEach(async (ele) => {
+        await navigator
+            .getBattery()
+            .then(function (battery) {
+                ele.innerHTML = (ele.getAttribute("no") !== null) ? battery.level * 100 : battery.level * 100 + "%";
+            })
+            .catch(function (e) {
+                ele.innerHTML = "Your browser app does not support the battery api !";
+            });
+    });
